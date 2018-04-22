516f3944ce170f65b607b0a0dee5c94385d302b9
Leilani Battle <leibatt@.mit.edu>
2018-04-22 18:11:02 -0400

    adding multiple displays

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -15,6 +15,10 @@ public class GameManager : MonoBehaviour {
 
     // Use this for initialization
     void Start () {
+        if(Display.displays.Length > 1)
+        {
+          Display.displays[1].Activate();
+        }
         Pineapple.transform.position = generateRandomCoords(stuffRadius) + new Vector3(0, 1.5f, 0);
         RandomMinePlacement(NumberMines);
 	}
